1093adeb372d942e9944f025f9dbeae7d42cc3da
macrosalt <liyan@usc.edu>
2022-08-05 21:44:02 +0800

    Delete env directory

--- env/lib/python3.8/site-packages/func_timeout/py2_raise.py
+++
@@ -1,5 +0,0 @@
-
-
-# Python2 allows specifying an alternate traceback.
-def raise_exception(exception):
-    raise exception[0] , None , exception[0].__traceback__
--- env/lib/python3.8/site-packages/ipapi/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ['ipapi']
-from .ipapi import location
