9182fc635ce75724b4c90b92a03b00477d1437bb
Yan Li <salt.liyan@shopee.com>
2022-08-06 21:01:17 +0800

    fix scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
+#!/bin/sh
 sudo apt-get update -y
 sudo apt-get install -y xvfb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install chromium-chromedriver
-
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y chromium-chromedriver
 sudo apt-get -y install python3-pip
 pip3 install -r requirements.txt
