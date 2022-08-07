7ffeb35b18975d0bb67f80afc381d7eb51f21f5f
Yan Li <salt.liyan@shopee.com>
2022-08-07 23:08:49 +0800

    fix py lib dep

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install -y chromium-chromedriver
 sudo apt-get -y install python3-pip
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
+sudo pip3 uninstall --yes chardet
+sudo pip3 uninstall --yes urllib3
+sudo pip3 install --upgrade requests --no-input
 sudo touch accounts.json
