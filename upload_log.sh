b34eca39731a9d3c0fe9e44fa1e7f1978a09fbd8
Yan Li <salt.liyan@shopee.com>
2022-08-21 18:34:10 +0800

    fix log script

--- upload_log.sh
+++ upload_log.sh
@@ -2,7 +2,7 @@
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_NAME="${PROJECT_DIR}/logs/log_"$*""
 
-cd PROJECT_DIR
+cd ${PROJECT_DIR}
 git pull
 cp ~/Logs_accounts.txt ${LOG_NAME};
 git add ${LOG_NAME}
