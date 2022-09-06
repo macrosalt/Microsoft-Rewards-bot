9ec19d3e25b9cb1c8e18a4ee454863b60f259419
Yan Li <salt.liyan@shopee.com>
2022-09-06 22:35:35 +0800

    fix direct py script

--- .github/scripts/eligible_accounts.py
+++ .github/scripts/eligible_accounts.py
@@ -1,7 +1,17 @@
-import sys, json
+import json
+import os
 
 point_bar = 9100
-dict = json.load(sys.stdin)
-for key, val in dict.items():
-    if val["Points"] > point_bar:
-        print(f'account: {key} -> points:{val["Points"]}')
+log_prefix = "log"
+
+log_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../../../logs"))
+files= os.listdir(log_dir)
+for file in files: 
+    if os.path.isdir(file) or not file.startswith(log_prefix):
+        continue
+    with open(log_dir+"/"+file) as input:
+        dict = json.load(input)
+        for key, val in dict.items():
+            if val["Points"] > point_bar:
+                print(f'account: {key} -> points:{val["Points"]}')
+    
--- .github/scripts/illegal_tries.py
+++ .github/scripts/illegal_tries.py
@@ -1,7 +1,16 @@
-import sys, json
+import json
+import os
 
-dict = json.load(sys.stdin)
-for key, val in dict.items():
-    check_situation = val["Last check"]
-    if len(check_situation) != 10 or check_situation[0] != "2":
-        print(f'account: {key} -> check:{check_situation}')
+log_prefix = "log"
+
+log_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../../../logs"))
+files= os.listdir(log_dir)
+for file in files: 
+    if os.path.isdir(file) or not file.startswith(log_prefix):
+        continue
+    with open(log_dir+"/"+file) as input:
+        dict = json.load(input)
+        for key, val in dict.items():
+            check_situation = val["Last check"]
+            if len(check_situation) != 10 or check_situation[0] != "2":
+                print(f'account: {key} -> check:{check_situation} in {file}')
