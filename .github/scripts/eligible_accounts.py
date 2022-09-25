39e5e8e876dbb0f7961e5c5f008e97adf7ad3226
Yan Li <salt.liyan@shopee.com>
2022-09-25 20:36:26 +0800

    add instance info

--- .github/scripts/eligible_accounts.py
+++ .github/scripts/eligible_accounts.py
@@ -1,29 +1,24 @@
-from common import read_logs_to
+from common import read_logs_to, get_log_location,get_account_region
 
 REGION_POINT_BAR = {
     "sg": 9100,
     "us": 13000,
 }
 
-ACCOUNT_REGION = {
-    "saltleemsr052@outlook.com": "sg",
-    "drbvwltl@outlook.com": "sg",
-    "npmiygkue@outlook.com": "sg",
-}
 
-def get_account_region(account):
-    if account in ACCOUNT_REGION:
-        return ACCOUNT_REGION[account]
-    return "us"
+
+
 
 def get_eligible_accounts(obj):
     '''
     :input: LogFile
     '''
+    filename = obj.file_name
     json_obj = obj.json_obj
+    location = get_log_location(filename)
     for key, val in json_obj.items():
             region = get_account_region(key)
             if isinstance(val["Points"], int) and val["Points"] > REGION_POINT_BAR[region]:
-                print(f'account: {key} -> points:{val["Points"]} in {region}')
+                print(f'account: {key} -> points:{val["Points"]} from {location} in {region}')
 
 read_logs_to(get_eligible_accounts)
