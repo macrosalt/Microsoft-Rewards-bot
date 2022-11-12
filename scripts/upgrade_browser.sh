fa4b20c2f86613c3dd8b8983de6f4ba72d7c339d
Yan Li <salt.liyan@shopee.com>
2022-11-13 00:21:12 +0800

    add browser upgrade

---
+++ scripts/upgrade_browser.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get update -y
+sudo apt install -y google-chrome-stable
+sudo apt install -y chromium-chromedriver
--- upload_log.sh
+++ upload_log.sh
@@ -8,3 +8,5 @@ cp ~/Logs_accounts.txt ${LOG_NAME};
 git add ${LOG_NAME}
 git commit -m "[log]"$*""
 git push
+
+bash scripts/upgrade_browser.sh
