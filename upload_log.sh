b73ee1372eab79fb307af3e3c9b05906e0fee5b4
Yan Li <salt.liyan@shopee.com>
2023-05-01 17:39:56 +0800

    feat: clean large size log

--- upload_log.sh
+++ upload_log.sh
@@ -2,6 +2,7 @@
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_NAME="${PROJECT_DIR}/logs/log_"$*""
 
+# push log
 cd ${PROJECT_DIR}
 git pull
 cp ~/Logs_accounts.txt ${LOG_NAME};
@@ -9,4 +10,14 @@ git add ${LOG_NAME}
 git commit -m "[log]"$*""
 git push
 
+# update browser, driver version
 /bin/bash scripts/upgrade_browser.sh > browser.txt
+
+# clean large size log
+logfile=cron.log
+filesize=$(stat -c %s "$logfile")
+
+if [ "$filesize" -gt $((200 * 1024 * 1024)) ]; then
+    echo "Log file is larger than 200MB, deleting..."
+    rm "$logfile"
+fi
